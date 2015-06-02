2298fbc3be607e6bd9a2c7da72932968d5f92092
chaojunhou <cjhou1984@gmail.com>
2015-06-02 11:03:43 +0800

    tail and head

---
+++ Shell/README.md
@@ -0,0 +1,1 @@
+### The solution to all the Leetcode shell problems, update to the latest. All the codes have been ACed!
--- Shell/tenth_line.sh
+++ Shell/tenth_line.sh
@@ -1,1 +1,4 @@
-awk 'NR == 10' flow.dat 
+# Solution 1
+awk 'NR == 10' file.txt
+# Solution 2
+tail -n+10 file.txt | head -1 
